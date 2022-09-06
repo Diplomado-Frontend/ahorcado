47f8214cc82ff67c648dee8ce3472924bb9a2371
juanayala1 <juan.ayala@condorlabs.io>
2022-09-05 21:17:59 -0500

    add validation count words and intents

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,21 @@ import './App.css'
 
 function App() {
 
-  const [intent, setIntents] = useState(0);
-  const [words, setWords] = useState("HelloWorld");
+  const [intents, setIntents] = useState(0);
+  const [words, setWords] = useState(getRandomWords());
+  const [hideWord, setHideWord] = useState("_ ".repeat(words.length));
 
   //Word's validation
   const validateWords = (letter: string) => {
     console.log(letter);
-  }
+    if(words.includes(letter)){
+      // words+1;
+      setIntents(Math.min( intents + 1, 9));
+      // console.log("si existe")
+    } 
+  };
 
-    // const onHandledClickLetter = (e) => {
-    //     e.preventDefault();    
-    // }
-
+  
   return (
    <div className='text-center'>
 
@@ -28,7 +31,7 @@ function App() {
 
       { /* hidden word */ }
 
-      <h3 className="font-bold text-2xl my-4 text-blue-600"> - - - - - - </h3>
+      <h3 className="font-bold text-2xl my-4 text-blue-600"> { hideWord } </h3>
 
       { /* try count */ }
 
@@ -36,7 +39,7 @@ function App() {
 
       { /* message if lose */ }
 
-      <h3 className="font-bold text-2xl my-4 text-red-600">You are lose!! 😩 your word is: { getRandomWords() }</h3>
+      <h3 className="font-bold text-2xl my-4 text-red-600">You are lose!! 😩 your word is: { words }</h3>
 
       { /* message if win */ }
 
